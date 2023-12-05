3e62e2cdb213050bbf227fc5fbd80d2e3199194c
AleksandraJ22 <145792385+AleksandraJ22@users.noreply.github.com>
2023-12-05 15:42:13 +0100

    Update Offer.java

--- M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/bean/Offer.java
+++ M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/bean/Offer.java
@@ -14,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
+@Table(name="OFFER")
 public class Offer {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
