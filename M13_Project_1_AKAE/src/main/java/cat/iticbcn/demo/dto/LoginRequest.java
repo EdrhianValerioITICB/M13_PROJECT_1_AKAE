cd59d8390f89a347db5e6cfc47d5b3bd155f3ca4
Aqeedat-Nabi <145792532+Aqeedat-Nabi@users.noreply.github.com>
2023-12-14 16:26:52 +0100

    Add files via upload

--- M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/dto/LoginRequest.java
+++ M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/dto/LoginRequest.java
@@ -1,4 +1,6 @@
 package cat.iticbcn.demo.dto;
 
-public class LoginRequest {
+public record LoginRequest(String username, String password) {
+
+
 }
---
+++ M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/dto/UserRegisterDTO.java
@@ -0,0 +1,15 @@
+package cat.iticbcn.demo.dto;
+
+
+/*
+DTO with the information necessary to register a new user in the database{
+    "username": "user1",
+    "email": "user1@gmail.com",
+    "password": "user1pass1",
+    "password2": "user1pass2"
+}
+*/
+public record UserRegisterDTO(String username, String email, String password, String password2) {
+
+
+}
