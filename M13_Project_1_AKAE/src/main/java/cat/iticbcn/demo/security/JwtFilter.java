9ce1c7412773bb332f659c72fffddec2ec1948fc
Aqeedat-Nabi <145792532+Aqeedat-Nabi@users.noreply.github.com>
2023-12-14 16:27:45 +0100

    Add files via upload

---
+++ M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/security/JwtFilter.java
@@ -0,0 +1,59 @@
+package cat.iticbcn.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Extracts the JWT token from the Authorization header of the HTTP request.
+ */
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UserDetailsService userService;
+
+    @Override
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+
+        String token = this.extractToken((HttpServletRequest) request);
+
+        if (this.tokenProvider.isValidToken(token)) {
+
+            String username = this.tokenProvider.getUsernameFromToken(token);
+            UserDetails user = this.userService.loadUserByUsername(username);
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasLength(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring("Bearer ".length());
+        }
+        return null;
+    }
+
+}
