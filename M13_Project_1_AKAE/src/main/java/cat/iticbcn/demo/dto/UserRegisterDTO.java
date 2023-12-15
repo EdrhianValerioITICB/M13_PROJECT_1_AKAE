1d274398c89a9c7ab34137d908a68ccc54ec2cf9
Aqeedat-Nabi <145792532+Aqeedat-Nabi@users.noreply.github.com>
2023-12-15 14:55:37 +0100

    Update UserRegisterDTO.java

--- M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/dto/UserRegisterDTO.java
+++ M13_Project_1_AKAE/src/main/java/cat/iticbcn/demo/dto/UserRegisterDTO.java
@@ -6,7 +6,7 @@ DTO with the information necessary to register a new user in the database{
     "username": "user1",
     "email": "user1@gmail.com",
     "password": "user1pass1",
-    "password2": "user1pass2"
+    "password2": "user1pass1"
 }
 */
 public record UserRegisterDTO(String username, String email, String password, String password2) {
